d784064bcd14b921778d3a7487473e4567776be0
Arian Dapouyeh <Arian Dapouyeh>
2017-06-09 19:38:51 +0200

    !

--- lecture-55/js/lecture-55.js
+++ lecture-55/js/lecture-55.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",
 	function (event) {
     function answer (event) {
+    	console.log(event);
 	this.textContent = "submitted";
 	var country = document.getElementById("country").value;
 	var message = "<h2> You are from " + country + "! How nice! </h2>";
@@ -8,15 +9,24 @@ document.addEventListener("DOMContentLoaded",
 
 	// document.getElementById("content").textContent = message;
 	document.getElementById("content").innerHTML = message;
-	if (country === "denmark" && "europe" && "world" && "scandinavia") {
+	if (country === ("denmark")) {
 		document.getElementById("content").textContent = altMessage;
 		document.getElementById("title").textContent;
 		title = "I live in Denmark!"
 		document.querySelector("h1").textContent = title;
-	};	 
+	} 
+	else {
+		console.log(message);
+	}
 }
 // document.querySelector("button").addEventListener("click", answer);
 document.querySelector("#button").onclick = answer;
+document.querySelector("body").addEventListener("mousemove", function (event) {
+	if (event.altKey === true) {
+	console.log("x: " + event.clientX)
+	console.log("y: " + event.clientY)		
+	}
+});
 	}
 );
 
